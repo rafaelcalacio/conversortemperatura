6fbaba646db5e925ad03bd6cf027a09d5294e3e7
Rafael Calacio <calacioti@gmail.com>
2022-11-14 22:04:39 -0300

    subir arquivos

---
+++ Conversor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorTemperatura
+{
+    public class Conversor
+    {
+        public void ToCelcius()
+        {
+            Console.WriteLine("Digite a temperatura em Fahrenheit a ser convertida para Celcius:");
+            double Fahrenheit = Convert.ToDouble(Console.ReadLine());
+            double Celsius = (Fahrenheit - 32) * 5 / 9;
+            Console.WriteLine($"A temperatura em Celsius é {Celsius}");
+            Console.ReadLine();
+        }
+
+        public void ToFahrenheit()
+        {
+            Console.WriteLine("Digite a temperatura em Celsius a ser convertida para Fahrenheit:");
+            double Celsius = Convert.ToDouble(Console.ReadLine());
+            double Fahrenheit = (9/5 * Celsius) + 32;
+            Console.WriteLine($"A temperatura em Fahrenheit é {Fahrenheit}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ConversorTemperatura;
+
+Console.WriteLine($"1 - para converter Fahrenheit par Celsius" + Environment.NewLine +
+    "2 - para converter Celsius par Fahrenheit" + Environment.NewLine +
+    "3 - sair");
+
+string escolha = "0";
+
+Conversor conversor1 = new Conversor();
+while (escolha != "3")
+{
+    Console.WriteLine("Digite a opção desejada abaixo:");
+    escolha = Console.ReadLine();
+    switch (escolha)
+    {
+        case "1":
+            conversor1.ToCelcius();
+            break;
+        case "2":
+            conversor1.ToFahrenheit();
+            break;
+        default:
+            Console.WriteLine("Escolha uma opção válida!!!");
+            break;
+    }
+}
+
